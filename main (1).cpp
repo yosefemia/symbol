33c37f00ccf7555f73c1042e0ac3b8f0c30301d5
yosefemia <yosef.emia@hcdc.edu.ph>
2025-09-21 15:34:14 +0800

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string batman;
+    cout << "What is Bruce wayne known for: "; 
+    cin >> batman;
+    cout << "\n";
+    
+    cout << "oooooooooo             oooooooo" <<endl;
+    cout << " oooooooo   oo   oo   oooooooo" <<endl;
+    cout << "  ooooooo   ooooooo   ooooooo"  <<endl;
+    cout << "  ooooooooooooooooooooooooooo" <<endl;
+    cout << " ooooooooooooooooooooooooooooo" <<endl;
+    cout << "  ooooooooooooooooooooooooooo" <<endl;
+    cout << "      ooooooooooooooooooo" <<endl;
+    cout << "         ooooooooooooo" <<endl;
+    cout << "           ooooooooo"    <<endl;
+    cout << "             ooooo" <<endl;
+   
+
+    return 0;
+}
