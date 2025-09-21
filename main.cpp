62323faf20b12ee75a09a6545879c72a5c59db12
yosefemia <yosef.emia@hcdc.edu.ph>
2025-09-21 14:55:21 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string heart;
+    cout << "What organ pumps blood throughout the body: "; 
+    cin >> heart;
+    cout << "\n";
+    
+    cout << "    o   o   o   o     o   o   o   o   " <<endl;
+    cout << " o       o       o o       o       o  " <<endl;
+    cout << " o               o           o       o " <<endl;
+    cout << " o                           o     o " <<endl;
+    cout << "  o                         o   o " <<endl;
+    cout << "   o                     o   o " <<endl;
+    cout << "    o                 o o " <<endl;
+    cout << "     o           o o " <<endl;
+    cout << "      o       o " <<endl;
+    cout << "       o   o " <<endl;
+    cout << "         o " <<endl;
+    
+    cout << "  ooo     ooo        " <<endl;
+    cout << " ooooo   ooooo       " <<endl;
+    cout << "ooooooo ooooooo        " <<endl;
+    cout << " ooooooooooooo       " <<endl;
+    cout << "  ooooooooooo       " <<endl;
+    cout << "   ooooooooo       " <<endl;
+    cout << "    ooooooo       " <<endl;
+    cout << "     ooooo       " <<endl;
+    cout << "      ooo        " <<endl;
+    return 0;
+}
